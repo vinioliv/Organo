bfd8ef3845cfc2c249446562947ebc316812f287
Vinicius Soares Oliveira <vinicius.oliveira@soc.com.br>
2024-02-15 09:23:58 -0300

    feat: App

---
+++ organo/src/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+
+function App() {
+
+  const [colaboradores, setColaborador] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaborador([...colaboradores, colaborador])
+  }
+  return (
+    <div className="App">
+        <Banner/>
+        <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+    </div>
+  );
+}
+
+export default App;
